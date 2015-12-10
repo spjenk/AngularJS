4c1132fe4da3004ac2b30bfc3e56189486d28d25
Stephen Jenkinson <stephen.jenkinson@tattsgroup.com>
2015-12-10 14:33:50 +1000

    Basic github get

---
+++ basicRestGet/script.js
@@ -0,0 +1,27 @@
+(function() {
+
+  var app = angular.module("githubViewer", []);
+
+  var MainController = function($scope, $http) {
+
+    var onUserComplete = function(response) {
+      $scope.user = response.data;
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch the data";
+    };
+
+    $scope.search = function(username) {
+      $http.get("https://api.github.com/users/" + username)
+        .then(onUserComplete, onError);
+    }
+
+    $scope.username = "angular"
+    $scope.message = "Github Viewer"
+
+  };
+
+  app.controller("MainController", ["$scope", "$http", MainController]);
+
+}());
