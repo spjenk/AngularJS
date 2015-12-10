8ff05f9c054a4668e8ee8c6d391c815666b1b382
Stephen Jenkinson <stephen.jenkinson@tattsgroup.com>
2015-12-10 15:11:23 +1000

    Add repeat and show/hide

--- basicRestGet/script.js
+++ basicRestGet/script.js
@@ -5,8 +5,13 @@
   var MainController = function($scope, $http) {
 
     var onUserComplete = function(response) {
-      $scope.user = response.data;
+        $scope.user = response.data;
+        $http.get($scope.user.repos_url).then(onRepos, onError);
     };
+
+    var onRepos  = function(response) {
+        $scope.repos = response.data;
+    }
 
     var onError = function(reason) {
       $scope.error = "Could not fetch the data";
@@ -19,7 +24,7 @@
 
     $scope.username = "angular"
     $scope.message = "Github Viewer"
-
+    $scope.repoSortOrder = "-stargazers_count";
   };
 
   app.controller("MainController", ["$scope", "$http", MainController]);
