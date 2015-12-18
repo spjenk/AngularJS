a9ac963517911b34f830e95e50b245ad04a3d8a0
Stephen Jenkinson <stephen.jenkinson@tattsgroup.com>
2015-12-18 16:47:11 +1000

    Get Offers for multi and filter for Head to Head

--- ubetSport/multi/script.js
+++ ubetSport/multi/script.js
@@ -1,29 +1,36 @@
 (function() {
 
-  var app = angular.module("githubViewer", []);
+  var app = angular.module("nrlMultiViewer", []);
 
   var MainController = function($scope, $http) {
   
-  $http.get('https://api.ubet.com/sales/vmax/web/data/sports/meeting/4339').success(function(data) {
+  $http.get('https://api.ubet.com/sales/vmax/web/data/sports/meeting/424').success(function(data) {
             $scope.sportData = data;
-			$scope.mainEvents = data.Sport.Leagues[0].Meetings[0].MainEvents;
-        });   
-
-    $scope.isResultType = function(subEvent) {
-		return subEvent.BetTypeId == 91;
-	};
+			var mainEvents = data.Sport.Leagues[0].Meetings[0].MainEvents;
+			for (me in mainEvents) {
+				$http.get("https://api.ubet.com/sales/vmax/web/data/sports/mainevent/" + mainEvents[me].MainEventId)
+				.then(onOffer, onError);
+			}
+        });		
 	
-	var onSportComplete = function(response) {
-        $scope.sport = response.data;        
+	var onOffer = function(response) {					
+        var subEvents = response.data.MainEvent.SubEvents;
+        for (subEvent in subEvents) {
+			var se  = subEvents[subEvent];
+			if (se.BetTypeId ==  72) {
+				$scope.subEvents.push(se);
+			}
+		}
     };
-
-    var onError = function(reason) {
+	
+	var onError = function(reason) {
       $scope.error = "Could not fetch the data";
     };
 
     $scope.message = "Sports Multi"
+	$scope.subEvents  = []
   };
-
+  
   app.controller("MainController", ["$scope", "$http", MainController]);
 
 }());
