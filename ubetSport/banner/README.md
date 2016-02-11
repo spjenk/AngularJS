79495d2584bd01c5c1750d0755e4459eb3a8eb9a
Stephen Jenkinson <stephen.jenkinson@gmail.com>
2016-02-11 20:53:04 +1000

    Add example for getting basic data to display head to head odds for an NRL game

---
+++ ubetSport/banner/README.md
@@ -0,0 +1,5 @@
+# Basic data retrieve
+
+Example of a basic angular div that gets head to head odds for a NRL game. 
+
+Data could be used for showing UBet odds on a webpage
---
+++ ubetSport/banner/ubetBannerScript.js
@@ -0,0 +1,19 @@
+(function() {
+
+  var app = angular.module("ubetBanner", []);
+
+  var MainController = function($scope, $http) {
+  
+  $http.get('https://api.ubet.com/sales/vmax/web/data/sports/meeting/mainevent/subevent/2262007').success(function(data) {
+            $scope.sportData = data;
+			$scope.ubetOffers = data.Sport.Leagues[0].Meetings[0].MainEvents[0].SubEvents[0].Offers;
+        });
+	
+	var onError = function(reason) {
+      $scope.ubetError = "Could not fetch the data";
+    };
+  };
+  
+  app.controller("MainController", ["$scope", "$http", MainController]);
+
+}());
