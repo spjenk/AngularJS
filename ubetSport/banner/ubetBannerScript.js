4d0d7d45fbb455ceb88337fd80c18c16dc54dec2
Stephen Jenkinson <stephen.jenkinson@gmail.com>
2016-02-12 09:32:21 +1000

    reformat banner and moved script from header to inside div wrapper

--- ubetSport/banner/ubetBannerScript.js
+++ ubetSport/banner/ubetBannerScript.js
@@ -1,19 +1,19 @@
-(function() {
+(function () {
 
-  var app = angular.module("ubetBanner", []);
+    var app = angular.module("ubetBanner", []);
 
-  var MainController = function($scope, $http) {
-  
-  $http.get('https://api.ubet.com/sales/vmax/web/data/sports/meeting/mainevent/subevent/2262007').success(function(data) {
+    var MainController = function ($scope, $http) {
+
+        $http.get('https://api.ubet.com/sales/vmax/web/data/sports/meeting/mainevent/subevent/2262007').success(function (data) {
             $scope.sportData = data;
-			$scope.ubetOffers = data.Sport.Leagues[0].Meetings[0].MainEvents[0].SubEvents[0].Offers;
+            $scope.ubetOffers = data.Sport.Leagues[0].Meetings[0].MainEvents[0].SubEvents[0].Offers;
         });
-	
-	var onError = function(reason) {
-      $scope.ubetError = "Could not fetch the data";
+
+        var onError = function (reason) {
+            $scope.ubetError = "Could not fetch the data";
+        };
     };
-  };
-  
-  app.controller("MainController", ["$scope", "$http", MainController]);
+
+    app.controller("MainController", ["$scope", "$http", MainController]);
 
 }());
