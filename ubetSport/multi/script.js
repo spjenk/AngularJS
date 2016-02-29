5a4db2a295f7299a839cdde6a8abfc26eb470b60
Stephen Jenkinson <stephen.jenkinson@gmail.com>
2016-02-29 21:30:03 +1000

    Filter for open events only. Add dynamic url to ubet.com

--- ubetSport/multi/script.js
+++ ubetSport/multi/script.js
@@ -1,36 +1,39 @@
-(function() {
+(function () {
 
-  var app = angular.module("nrlMultiViewer", []);
+    var app = angular.module("nrlMultiViewer", []);
 
-  var MainController = function($scope, $http) {
-  
-  $http.get('https://api.ubet.com/sales/vmax/web/data/sports/meeting/424').success(function(data) {
+    var MainController = function ($scope, $http) {
+
+        var H2H = 72;
+        var OPEN = "o";
+
+        $http.get('https://api.ubet.com/sales/vmax/web/data/sports/meeting/424').success(function (data) {
             $scope.sportData = data;
-			var mainEvents = data.Sport.Leagues[0].Meetings[0].MainEvents;
-			for (me in mainEvents) {
-				$http.get("https://api.ubet.com/sales/vmax/web/data/sports/mainevent/" + mainEvents[me].MainEventId)
-				.then(onOffer, onError);
-			}
-        });		
-	
-	var onOffer = function(response) {					
-        var subEvents = response.data.MainEvent.SubEvents;
-        for (subEvent in subEvents) {
-			var se  = subEvents[subEvent];
-			if (se.BetTypeId ==  72) {
-				$scope.subEvents.push(se);
-			}
-		}
-    };
-	
-	var onError = function(reason) {
-      $scope.error = "Could not fetch the data";
+            var mainEvents = data.Sport.Leagues[0].Meetings[0].MainEvents;
+            for (me in mainEvents) {
+                $http.get("https://api.ubet.com/sales/vmax/web/data/sports/mainevent/" + mainEvents[me].MainEventId)
+                    .then(onOffer, onError);
+            }
+        });
+
+        var onOffer = function (response) {
+            var subEvents = response.data.MainEvent.SubEvents;
+            for (subEvent in subEvents) {
+                var se = subEvents[subEvent];
+                if (se.BetTypeId == H2H && se.Status == OPEN) {
+                    $scope.subEvents.push(se);
+                }
+            }
+        };
+
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the data";
+        };
+
+        $scope.message = "Sports Multi"
+        $scope.subEvents = []
     };
 
-    $scope.message = "Sports Multi"
-	$scope.subEvents  = []
-  };
-  
-  app.controller("MainController", ["$scope", "$http", MainController]);
+    app.controller("MainController", ["$scope", "$http", MainController]);
 
 }());
