fcd378437c4d4a6e1acdd0b9139d12b389946b34
Stephen Jenkinson <stephen.jenkinson@gmail.com>
2016-03-08 15:35:52 +1000

    Added League level call to always get the active matches for a round

--- ubetSport/multi/script.js
+++ ubetSport/multi/script.js
@@ -6,15 +6,25 @@
 
         var H2H = 72;
         var OPEN = "o";
+        var MATCH = "M";
 
-        $http.get('https://api.ubet.com/sales/vmax/web/data/sports/meeting/424').success(function (data) {
-            $scope.sportData = data;
-            var mainEvents = data.Sport.Leagues[0].Meetings[0].MainEvents;
+        $http.get('https://api.ubet.com/sales/vmax/web/data/sports/league/48').success(function (data) {
+            var meetings = data.Sport.Leagues[0].Meetings;
+            for (blah in meetings) {
+                if (meetings[blah].MatchRoundSeason == MATCH) {
+                    $http.get("https://api.ubet.com/sales/vmax/web/data/sports/meeting/" + meetings[blah].MeetingId)
+                        .then(onMeeting, onError);
+                }
+            }
+        });
+
+        var onMeeting = function (response) {
+            var mainEvents = response.data.Sport.Leagues[0].Meetings[0].MainEvents;
             for (me in mainEvents) {
                 $http.get("https://api.ubet.com/sales/vmax/web/data/sports/mainevent/" + mainEvents[me].MainEventId)
                     .then(onOffer, onError);
             }
-        });
+        }
 
         var onOffer = function (response) {
             var subEvents = response.data.MainEvent.SubEvents;
@@ -32,6 +42,7 @@
 
         $scope.message = "Sports Multi"
         $scope.subEvents = []
+        $scope.userSelection = {};
     };
 
     app.controller("MainController", ["$scope", "$http", MainController]);
